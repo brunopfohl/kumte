ace72430fb8319935abb444a957537ebe497b551
bruno.pfohl <bruno.pfohl@seznam.cz>
2025-04-16 23:59:05 +0200

    remove unnecessary quiz screen

--- App/App.tsx
+++ App/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -12,7 +5,6 @@ import { RootStackParamList } from './src/types';
 import { LibraryScreen } from './src/screens/LibraryScreen';
 import { CameraScreen } from './src/screens/CameraScreen';
 import { ViewerScreen } from './src/screens/ViewerScreen';
-import QuizScreen from './src/screens/QuizScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -32,10 +24,6 @@ export default function App() {
           <Stack.Screen 
             name="Viewer" 
             component={ViewerScreen}
-          />
-          <Stack.Screen 
-            name="Quiz" 
-            component={QuizScreen}
           />
         </>
       } />
